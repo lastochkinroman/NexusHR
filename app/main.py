1e37c8fc37ae558b404bb55b9140bb57336a3884
Lastochkin <rdlastochkin@gmail.com>
2026-02-19 17:46:28 +0400

    chore: initial project structure and config setup

---
+++ app/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from app.core.config import settings
+
+app = FastAPI(title=settings.PROJECT_NAME)
+
+@app.get("/health")
+def health_check():
+    return {
+        "status": "ok", 
+        "project_name": settings.PROJECT_NAME,
+        "details": "Server is running!"
+    }
